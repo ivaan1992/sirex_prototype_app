8ae4cb5e3f6e463e7f46c44c6e62852b5a1c6c4d
Ivan <ivaan.lg.92@gmail.com>
2023-07-26 18:52:11 -0600

    Start creating routes and components

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <>
+            <header>
+                <h1>Este es el encabezado principal que debe ir en todas las vistas!!</h1>
+            </header>
+        </>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
+import Header from "~/components/Header";
+
 export default function Home() {
 
   return (
     <>
+      <Header />
       <h1>Sirex prototype app</h1>
-
-      
     </>
   );
 }
